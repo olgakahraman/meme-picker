85f7a2f9c974e80ea8dc2237b2396db8133b69a2
olgakahraman <ol.khrmn@gmail.com>
2023-09-26 14:27:15 -0400

    removed the highlight class

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const emotionRadios = document.querySelector("#emotion-radios");
 emotionRadios.addEventListener('change', highlightCheckedOption)
 
 function highlightCheckedOption(e){
+const radios = document.getElementsByClassName('radio');
+for (let radio of radios){
+    radio.classList.remove('highlight');
+}
 
-   document.getElementById(e.target.id).classList.add('highlight');
+   document.getElementById(e.target.id).parentElement.classList.add('highlight');
 
-
-  
 }
 
 
