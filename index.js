aff6689133175b78a211db161c8a30f3df4b1dcd
olgakahraman <ol.khrmn@gmail.com>
2024-04-17 11:30:58 -0400

    connected to getImageBtn to checked value and console.log it

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
 import { catsData } from "/data.js";
+
 const emotionRadios = document.querySelector("#emotion-radios");
+const getImageBtn = document.querySelector("#get-image-btn");
 
-emotionRadios.addEventListener('change', highlightCheckedOption)
+emotionRadios.addEventListener("change", highlightCheckedOption);
 
-function highlightCheckedOption(e){
-const radios = document.getElementsByClassName('radio');
-for (let radio of radios){
-    radio.classList.remove('highlight');
+getImageBtn.addEventListener("click", getMatchingCatsArray);
+
+function highlightCheckedOption(e) {
+  const radios = document.getElementsByClassName("radio");
+  for (let radio of radios) {
+    radio.classList.remove("highlight");
+  }
+
+  document.getElementById(e.target.id).parentElement.classList.add("highlight");
 }
 
-   document.getElementById(e.target.id).parentElement.classList.add('highlight');
-
+function getMatchingCatsArray() {
+  const selectedEmotion = document.querySelector(
+    'input[type="radio"]:checked'
+  ).value;
+  console.log(selectedEmotion);
 }
-
 
 function getEmotionsArray(cats) {
   const emotionsArray = [];
   for (let cat of cats) {
     for (let emotion of cat.emotionTags) {
-
-        if (!emotionsArray.includes(emotion)){
-            emotionsArray.push(emotion);   
-        }
-
-      
+      if (!emotionsArray.includes(emotion)) {
+        emotionsArray.push(emotion);
+      }
     }
   }
 
@@ -34,7 +40,6 @@ function renderEmotionsRadios(cats) {
   let radioItems = ``;
   const emotions = getEmotionsArray(cats);
   for (let emotion of emotions) {
-    
     radioItems += `
     <div class="radio">
       <label for = "${emotion}">${emotion}</label>
